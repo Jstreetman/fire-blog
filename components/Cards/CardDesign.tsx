b3a52b8764f8a5c9dacfd4277adc87f43ffa0585
Jonathan Streetman <jonathanstreetman@Jonathans-MacBook-Air-2.local>
2024-05-23 12:12:14 -0400

    Card Animations

--- components/Cards/CardDesign.tsx
+++ components/Cards/CardDesign.tsx
@@ -15,16 +15,34 @@ const ShimmyCard = (props: any) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 1.3, ease: "easeInOut" }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
       className="group relative mx-auto w-full max-w-sm overflow-hidden rounded-lg bg-slate-800 p-0.5 transition-all duration-500 hover:scale-[1.01] hover:bg-slate-800/50"
     >
       <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-slate-900 p-8 transition-colors duration-500 group-hover:bg-slate-800">
-        <GiArabicDoor className="relative z-10 mb-10 mt-2 rounded-full border-2 border-blue-500 bg-slate-900 p-4 text-7xl text-blue-500" />
+        <motion.div
+          initial={{ opacity: 0, scale: 2 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+        >
+          <GiArabicDoor className="relative z-10 mb-10 mt-2 rounded-full border-2 border-blue-500 bg-slate-900 p-4 text-7xl text-blue-500" />
+        </motion.div>
 
-        <h4 className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">
+        <motion.h4
+          initial={{ opacity: 0, x: -1000 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.3, ease: "easeInOut" }}
+          className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50"
+        >
           {props.title}
-        </h4>
-        <p className="relative z-10 text-slate-400">{props.description}</p>
+        </motion.h4>
+        <motion.p
+          initial={{ opacity: 0, x: 1000 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.6, ease: "easeInOut" }}
+          className="relative z-10 text-slate-400"
+        >
+          {props.description}
+        </motion.p>
       </div>
 
       <motion.div
