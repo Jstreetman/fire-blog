10cae78f8c57152f0c74b6ea2c2c7e47fd95fd8e
Jonathan Streetman <jonathanstreetman@Jonathans-MacBook-Air-2.local>
2024-05-22 21:40:27 -0400

    pre landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Cards } from "@/components/Cards/Cards";
+import { DarkGridHero } from "@/components/Landing/Hero/PreLanding";
 
 export default function Home() {
   return (
     <div>
-      <Cards />
+      <DarkGridHero />
+      {/* //<Cards /> */}
     </div>
   );
 }
